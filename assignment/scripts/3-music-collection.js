5f88c8b5acb1db39d38d3e38fc9d033af6b48779
Tiffany House-Jimenez <tiffanyhousejimenez@gmail.com>
2023-11-26 15:13:18 -0600

    created addToCollection function wiwth new object title newMusic

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,12 +1,58 @@
 console.log('***** Music Collection *****')
 // Safe Zone -- Write code below this line
 
-//create an empty array
-const myCollection = [];
+//create an empty array named myCollection
+let myCollection = [];
 
-// Create a function named addToCollection: collection, title, artist, yearPublished 
+// Create a function named addToCollection with parameters collection, title, artist, yearPublished 
 
-function addToCollection{myCollection, title, artist, yearPublished}
+function addToCollection(collection, title, artist, yearPublished){
+
+  const newMusic = { //take in the albums title, artist, yearPublished as parameters and create a new object
+    title: title,
+    artist: artist,
+    yearPublished: yearPublished,
+  };
+  collection.push(newMusic); //add the new object to the end of the collection array
+return newMusic;
+};
+
+
+//Use and Test the addToCollection function:
+
+
+
+
+
+
+//Add 6 albums to the myCollection array. Aim to have a mix of both same and different artists and published years. (Feel free to share your musical interests, or make stuff up. Totally fine either way.)
+//console.log each album as added using the function's returned value.
+//After all are added, console.log the myCollection array.
+  
+
+
+
+
+//return the newly created object
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
